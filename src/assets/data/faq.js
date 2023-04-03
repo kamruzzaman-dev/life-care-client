45acf9aff6ed1e2dca56eb3f811654cb22c60f68
md kamruzzaman <kamruzzamanmd@hotmail.com>
2023-04-03 16:03:31 +0600

    faq section is added.

---
+++ src/assets/data/faq.js
@@ -0,0 +1,59 @@
+export const faqList = [
+    {
+      id: "suerlsreui",
+      ques: "What is a blockchain oracle?",
+      ans: "A blockchain oracle is any system that services a smart contract by providing it with data from an off-chain source or connecting it with an off-chain system. Oracles enable connectivity between blockchains and real-world data by interfacing with external APIs and data feeds, allowing them to pull data for or push data from a smart contract.",
+    },
+    {
+      id: "f4ser7s7er7",
+      ques: "Why do blockchains need oracles?",
+      ans: "Oracles enable smart contracts to interact with off-chain resources. Without oracles, smart contracts can only access data within a blockchain, severely limiting their potential applications. The majority of industry-changing real-world use cases for smart contracts require off-chain data and computation for proper execution and often an integration with existing payment rails to settle a contract. For example, a smart contract may require high-quality weather data from a reputable source like AccuWeather to trigger a transaction. It may also require the ability to pay out escrowed fiat currency through existing financial infrastructure, such as a bank checking account."
+    },
+    {
+      id: "se7r8sre8s8erf",
+      ques: "What is the medical Network?",
+      ans: "The Chainlink Network refers to all of the decentralized oracle networks actively operating using the Chainlink protocol, including individual oracles, data providers running their own node, and node operators. By enabling smart contracts to quickly and securely connect to off-chain data sources using decentralized oracle networks, the Chainlink Network helps developers build more robust and diverse blockchain applications using real-world inputs and outputs. Additionally, as middleware between low-level blockchain protocols and external, off-chain data sources, the Chainlink Network serves as an abstraction layer for data providers to easily sell their existing APIs to any blockchain network."
+    },
+]
+// eth data
+  export const ETHData = [
+    {
+      id: "4d55f",
+      time: "12:30 AM",
+      amount: "487",
+      hash: "4s8er5s5fe57rjmxnfuewrurks"
+    },
+    {
+      id: "ser7s7er",
+      time: "10:30 AM",
+      amount: "875",
+      hash: "sf7s7ers4e7r7wser"
+    },
+    {
+      id: "s77se7r",
+      time: "11:30 AM",
+      amount: "797",
+      hash: "se4s7er7"
+    },
+]
+// eth data
+  export const TestLinkData = [
+    {
+      id: "s7e7r",
+      time: "08:30 AM",
+      amount: "748",
+      hash: "7s7effkeurusue4"
+    },
+    {
+      id: "s7fe7r",
+      time: "10:23 AM",
+      amount: "974",
+      hash: "sfe7r7sr4fer"
+    },
+    {
+      id: "ser8ser7",
+      time: "11:10 AM",
+      amount: "874",
+      hash: "s4e7s8er"
+    },
+]
